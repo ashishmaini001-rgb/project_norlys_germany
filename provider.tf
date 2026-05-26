da8f3271f8aebea32760716e762128a84606c804
Ashish <ashish.maini00111@gmail.com>
2026-05-26 14:19:19 +0530

    code update

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  
+required_providers {
+  azurerm ={
+
+    source = "hasicorp/azurerm"
+    version = "4.37.0"
+  }
+
+}
+
+}
---
+++ rg_norlys.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "block1" {
+
+    name ="speed"
+    location ="West US"  
+}
